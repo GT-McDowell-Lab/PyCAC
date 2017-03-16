135afeafd24b884b069a3a4a939b74ba14ec46ec
shuozhixu <shuozhixu@gmail.com>
2017-03-16 18:20:07 -0400

    Update docs

--- docs/_book/publish_gitbook.sh
+++ docs/_book/publish_gitbook.sh
@@ -7,12 +7,6 @@ cd /Users/shuozhixu/Public/github/PyCAC/docs/
 # install the plugins and build the static site
 gitbook install && gitbook build
 
-# checkout to the gh-pages branch
-git checkout gh-pages
-
-# pull the latest updates
-#git pull origin gh-pages --rebase
-
 # copy the static site files into the current directory.
 cp -R _book/* .
 
@@ -26,10 +20,13 @@ git add .
 # commit
 git commit -a -m "Update docs"
 
-# push to the origin
+git push origin master
+
+git checkout gh-pages
+
+git rebase master
+
 git push origin gh-pages
 
 # checkout to the master branch
 git checkout master
-
-git rebase gh-pages
