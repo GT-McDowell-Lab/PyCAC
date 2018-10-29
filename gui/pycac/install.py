667e4c3030babccba31f7ae1c79b1dcf1cf038f9
Kevin C <chu.k@gatech.edu>
2018-10-29 17:37:02 -0400

    Added license and copyright headers

--- gui/pycac/install.py
+++ gui/pycac/install.py
@@ -1,14 +1,22 @@
-#!/usr/bin/env python
-#title:install.py
-#description:install cac from source onto user cluster
-#					
-#author:chu.k@gatech.edu
-#updated:2018/06/18
-#version:0.2
-#usage:N/A
-#notes:McDowell Lab PyCAC. Version req for dictionary order preservation
-#python_version:3.6.4
-#=============================================================================
+'''
+    Copyright 2018, Georgia Institue of Technology (C)
+
+    This file is part of PyCAC.
+
+    PyCAC is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PyCAC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PyCAC.  If not, see <https://www.gnu.org/licenses/>.
+
+'''
 from .ssh_util import upload, login
 import os, sys, json
 from time import sleep
